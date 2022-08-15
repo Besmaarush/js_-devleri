c6246b78df4e623f8ea66bfeb644ce119c3a55b6
Besmaarush <basma.aroush2016@gmail.com>
2022-08-15 21:20:42 +0300

    first commit

---
+++ 1.week/index.js
@@ -0,0 +1,24 @@
+//ebob
+const ebob = (a, b) => {
+    while (a !== 0 && b !== 0) {
+        if (a > b) a %= b;
+        else b %= a;
+    }
+    return a + b;
+};
+
+//ekok
+const ekok = (a, b) => a * b / ebob(a, b);
+
+const numb = n => {
+    if (n < 3) return n;
+    let acc = 2;
+    for (let i = 3; i <= n; i++) {
+        acc = ekok(acc, i);
+    }
+    return acc;
+};
+
+
+console.log(numb(20));
+
---
+++ 2.week/README.md
@@ -0,0 +1,3 @@
+# Tic-Tac-Toe App
+
+![Tic-Tac-Toe](https://user-images.githubusercontent.com/101412521/177860848-e445f464-7b3a-45bd-8945-977e67ec7ea3.png)
